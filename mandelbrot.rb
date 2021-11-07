b4c36b66a31a2d6e05697b57181e52c18cd0ed68
Jonas Arvidsson <jonas054@gmail.com>
2021-11-07 11:00:17 +0100

    Refactor and fiddle

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -6,7 +6,7 @@ require 'rainbow'
 
 NR_OF_THREADS = 5
 SCALE = 1
-UPDATES_BETWEEN_CALCULATIONS = 15
+UPDATES_BETWEEN_CALCULATIONS = 8
 ZOOM_FACTOR = 0.6
 
 # Mandelbrot set viewer
@@ -23,72 +23,77 @@ class Mandelbrot < Gosu::Window
 
   def update
     start = Time.now
-    sleep 0.1
     if @updates_left_before_calc < 2
-      if @threads
-        @threads.each(&:join)
-        @result_to_draw = @last_result = @result
-        @size *= ZOOM_FACTOR
-        @max *= 1.2
-      end
-      @result = Array.new(height) { [nil] * width }
-      @threads = NR_OF_THREADS.times.map do |ix|
-        Thread.new(ix) { |i| executor(i, @size, @x_offset, @y_offset, @max) }
-      end
+      wait_for_calculation if @threads
+      start_new_calculation
       @updates_left_before_calc = UPDATES_BETWEEN_CALCULATIONS
+      puts Rainbow("calculate: #{Time.now - start}").green
     else
       @updates_left_before_calc -= 1
-      if @last_result
-        exp = 1 - @updates_left_before_calc.to_f / UPDATES_BETWEEN_CALCULATIONS
-        zoom = ZOOM_FACTOR ** exp
-        margin = (1 - zoom) / 2
-        y_offset = height * margin
-        x_offset = width * margin
-        @result_to_draw = Array.new(height) { [nil] * width }
-        (0...height).each do |y|
-          (0...width).each do |x|
-            @result_to_draw[y][x] =
-              @last_result[y_offset + y * zoom][x_offset + x * zoom]
-          end
-        end
-      end
+      zoom_in if @last_result
+      puts Rainbow("zoom: #{Time.now - start}").yellow
     end
-    mode = @updates_left_before_calc == UPDATES_BETWEEN_CALCULATIONS ?
-             :calculate : :zoom
-    puts Rainbow(mode => Time.now - start).yellow
   end
 
   def draw
+    return unless @result_to_draw
+
     start = Time.now
-    sleep 0.1
     @hue_offset += 1
 
-    unless @result_to_draw && @result_to_draw[height - 1]
-      puts Rainbow("Not ready to draw").red
-      return
-    end
-
     puts "X:#{@x_offset} Y:#{@y_offset} S:#{@size} M:#{@max.to_i}"
-    (0...height).each do |y|
+    @result_to_draw.each_with_index do |row, y|
       x = 0
       while x < width
-        value = @result_to_draw[y][x]
-        color = if value >= @max - 1
-                  Gosu::Color::BLACK
-                else
-                  Gosu::Color.from_hsv((@hue_offset + 360 * value / @max) % 360,
-                                       1.0, 0.5 - 1.0 * value / @max / 2)
-                end
-        line_length = 0
-        line_length += 1 while x + line_length < width && @result_to_draw[y][x + line_length] == @result_to_draw[y][x]
-        draw_rect(x, y, line_length, 1, color)
-        x += line_length
+        value = row[x]
+        x1 = x + 1
+        x1 += 1 while x1 < width && row[x1] == value
+        draw_rect(x, y, x1 - x, 1, color_of(value))
+        x = x1
       end
     end
-    puts Rainbow(draw: Time.now - start).cyan
+    puts Rainbow("draw: #{Time.now - start}").cyan
   end
 
   private
+
+  def wait_for_calculation
+    @threads.each(&:join)
+    @result_to_draw = @last_result = @result
+    @size *= ZOOM_FACTOR
+    @max *= 1.2
+  end
+
+  def start_new_calculation
+    @result = Array.new(height) { [nil] * width }
+    @threads = NR_OF_THREADS.times.map do |ix|
+      Thread.new(ix) { |i| executor(i, @size, @x_offset, @y_offset, @max) }
+    end
+  end
+
+  def zoom_in
+    zoom =
+      ZOOM_FACTOR**(1 - @updates_left_before_calc.to_f / UPDATES_BETWEEN_CALCULATIONS)
+    margin = (1 - zoom) / 2
+    y_offset = height * margin
+    x_offset = width * margin
+    @result_to_draw = Array.new(height) { [nil] * width }
+    (0...height).each do |y|
+      (0...width).each do |x|
+        @result_to_draw[y][x] =
+          @last_result[y_offset + y * zoom][x_offset + x * zoom]
+      end
+    end
+  end
+
+  def color_of(value)
+    if value >= @max - 1
+      Gosu::Color::BLACK
+    else
+      Gosu::Color.from_hsv((@hue_offset + 360 * value / @max) % 360,
+                           1.0, 0.5 - 1.0 * value / @max / 2)
+    end
+  end
 
   def executor(index, size, x_offset, y_offset, max)
     height.times do |row|
